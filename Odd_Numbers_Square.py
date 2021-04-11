dbe9b79c40ef8fc85b878200e9ba177ae8deb5b6
Yaman <husoyaman1987@gmail.com>
2021-04-11 03:11:53 +0300

    Listedeki Tek Sayıların Karesi

---
+++ Odd_Numbers_Square.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5, 6]
+new_list = []
+
+for x in my_list:
+  if x % 2 != 0:
+    new_list.append(pow(x,2))
+
+print(new_list)
+
+# [expression for item in list execution body if condition]
+
+my_list = [1, 2, 3, 4, 5, 6]
+print([x ** 2 for x in my_list if x % 2 != 0])
