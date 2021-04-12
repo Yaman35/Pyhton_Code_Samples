2ed490776c01ecbafe5635be24a3e4fa1bd1eeef
Yaman <husoyaman1987@gmail.com>
2021-04-12 17:12:42 +0300

    While Döngüsü ile Sayı Tahmin Etme Oyunu

---
+++ Guessing_The_Number.py
@@ -0,0 +1,17 @@
+# Sayı tahmin etme oyunu 
+
+answer = 44
+
+question = 'What number am I thinking of?:  '
+print ("Let's play the guessing game!")
+
+while True:
+    guess = int(input(question))
+
+    if guess < answer:
+        print('Little higher')
+    elif guess > answer:
+        print('Little lower')
+    else:  # guess == answer
+        print('Are you a MINDREADER!!!')
+        break
