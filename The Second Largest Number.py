7428d86c03caee357d47d31f28a6e77e3c9c78a8
Yaman <husoyaman1987@gmail.com>
2021-04-18 15:11:36 +0300

    Girilen Bir Listenin En Büyük İkinci Elemanını Bulma

---
+++ The Second Largest Number.py
@@ -0,0 +1,8 @@
+scores = input("Scores: ").split(",")
+scores2 = [int(scores[i]) for i in range(0, len(scores))]
+scores2.sort()
+print(scores2[-2])
+
+scores = input("Please enter a score array: ").split()
+arr = map(int, scores) # map() burada içine aldığı listenin her elemanını integer yapar, int yerine başka fonksiyon yazsaydık onu uygulardı
+print (sorted(set(arr))[-2])
