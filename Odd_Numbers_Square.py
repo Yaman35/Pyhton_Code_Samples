1a47766cb38e7ea74a078f7d1dc34a3a27a1e0bd
Hüseyin Yaman <81390395+Yaman35@users.noreply.github.com>
2021-04-18 15:57:46 +0300

    Verilen Bir Listedeki Tek Sayıların Karesini Alma

--- Odd_Numbers_Square.py
+++ Odd_Numbers_Square.py
@@ -12,4 +12,4 @@ print(new_list)
 # [expression for item in list execution body if condition]
 
 my_list = [1, 2, 3, 4, 5, 6]
-print([x ** 2 for x in my_list if x % 2 != 0])
+print([x ** 2 for x in my_list if x % 2 != 0]) 
