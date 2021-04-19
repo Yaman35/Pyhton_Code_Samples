a069f96e64c1002af90b89e3986284024492c23c
Yaman <husoyaman1987@gmail.com>
2021-04-19 22:00:48 +0300

    Girilen Sayının Rakamları Toplamını Bulma

--- Sum_of_The_Numbers.py
+++ Sum_of_The_Numbers.py
@@ -1,8 +1,32 @@
 # Girilen Sayının Rakamları Toplamını Bulma
 
+# Çözüm - 1
+
 number = int(input("Enter a number: "))
 toplam = 0
 while number > 0:
   toplam += number % 10
   number = int(number/10)
 print(toplam)  
+
+# Çözüm - 2 
+
+rakamlar = []
+for i in input("rakamları toplamını bulmak için bir sayı giriniz"):
+    rakamlar.append(int(i))
+print(sum(rakamlar))
+
+# Çözüm - 3
+
+sayi = int(input("Enter a number: "))
+top = 0
+for i in str(sayi):
+  top += int(i)
+print(top)
+
+# Çözüm - 4
+
+top=0
+for i in input("sayı giriniz"):
+  top += int(i)
+print(top)
